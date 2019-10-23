c755617f42d1fd5e6d1e8f9da817efe63310f553
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2019-10-23 18:46:46 -0400

    Print histogram

--- 13/freq.cpp
+++ 13/freq.cpp
@@ -10,19 +10,35 @@ using std::endl;
 #include <string>
 using std::string;
 
+void printStars (int n);
+void printHistogram(const map<string,int>& freq);
+
 int main(void){
 	map<string,int> freq;
 	string s;
 	cout << "Please enter some strings:" << endl;
+	//until end of stdin is reached
 	while(!cin.eof()){
 		cin >> s;
 		freq[s]++;
 	}
-	cout << "everything in map" << endl;
+	//in arch linux works
+	//in windows command prompt program is getting stuck here
 	for(map<string,int>::iterator i = freq.begin(); i != freq.end(); i++){
-		cout <<"in for" << endl;
 		cout << (*i).first << '\t' << (*i).second << endl;
-		cout << "after print" << endl;
 	}
+
+	printHistogram(freq);
 	return 0;
+}
+
+void printHistogram(const map<string,int>& freq){
+		for(map<string,int>::iterator i = freq.begin(); i != freq.end(); i++){
+		cout << (*i).first << '\t' << printStars((*i).second) << endl;
+	}
+
+}
+
+void printStars (int n){
+		cout << '*'*n;
 }
