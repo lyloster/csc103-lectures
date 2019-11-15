f153a7827a7ec8256906991a55a8ba2235069585
William E. Skeith <wes@cs.ccny.cuny.edu>
2019-11-14 21:00:55 -0500

    More notes.

---
+++ 20/merge-sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+void merge(const vector<int>& L, const vector<int>& R, vector<int>& M);
+
+/* TODO: try to write mergeSort from scratch.  See l5.pdf if you need
+ * some help. */
+
+int main()
+{
+	/* TODO: test out your merge sort here. */
+	return 0;
+}
+
+/* TODO: write a recursive function that prints the base 10 digits of n
+ * vertically to cout.  for example, printVertically(2358) would print
+ * 2
+ * 3
+ * 5
+ * 8
+ * Rules: you can't use any loops.  You can't use vectors or arrays.
+ * Just let the recursive function calls do the work for you.  */
